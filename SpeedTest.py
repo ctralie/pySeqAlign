64b5a2c70c662aa0b04c633b3afcee35a8224c23
Chris Tralie <chris.tralie@gmail.com>
2019-03-25 13:03:45 -0400

    Making speed test more rigorous

--- SpeedTest.py
+++ SpeedTest.py
@@ -1,5 +1,7 @@
 from PurePython import swconstrained as swnumba
 from pySeqAlign import swconstrained as swcython
+import seaborn as sns
+from scipy import stats
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sio
@@ -12,8 +14,10 @@ def getRandomCSM(N, M):
     D = np.array(D, dtype='double')
     return D
 
-np.random.seed(1)
-D = getRandomCSM(1000, 1000)
+np.random.seed(0)
+sizes = np.arange(8, 40)**2
+Ds = [getRandomCSM(s, s) for s in sizes]
+i = 0
 
 def compareTimes():
     """
@@ -24,14 +28,22 @@ def compareTimes():
     swnumba(getRandomCSM(10, 10))
     
     # Run cython code
-    cytime = timeit.timeit("swcython(D.flatten(), D.shape[0], D.shape[1])", number=reps, globals=globals())
-    cyans = swcython(D.flatten(), D.shape[0], D.shape[1])
+    cytimes = np.zeros(len(sizes))
+    nbtimes = np.zeros(len(sizes))
+    global i
+    for i, s in enumerate(sizes):
+        print("Doing %i x %i"%(s, s))
+        cytimes[i] = timeit.timeit("swcython(Ds[i].flatten(), Ds[i].shape[0], Ds[i].shape[1])", number=reps, globals=globals())
+        nbtimes[i] = timeit.timeit("swnumba(Ds[i])", number=reps, globals=globals())
+    sio.savemat("times.mat", {"cytimes":cytimes, "nbtimes":nbtimes})
+    
+    slope, intercept, r_value, p_value, std_err = stats.linregress(cytimes, nbtimes)
+    ax = sns.regplot(x=cytimes, y=nbtimes)
+    plt.legend(["y=%.3gx+%.3g (r=%.3g)"%(slope, intercept, r_value)])
+    plt.xlabel("Cython")
+    plt.ylabel("Numba")
+    plt.show()
 
-    nbtime = timeit.timeit("swnumba(D)", number=reps, globals=globals())
-    nbans = swnumba(D)
-
-    print("Cython Time: %.3g, ans=%.3g"%(cytime, cyans))
-    print("Numba  Time: %.3g, ans=%.3g"%(nbtime, nbans))
 
 if __name__ == "__main__":
     compareTimes()
